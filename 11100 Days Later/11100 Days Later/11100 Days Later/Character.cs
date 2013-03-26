56f9917ee4c43debfd2ef250e9581d5952f9899c
Samuel Parish <sirtalen01@googlemail.com>
2013-03-26 16:47:55 +0000

    Adding the basics for the game
    Background and character facings started
    input handling started

---
+++ 11100 Days Later/11100 Days Later/11100 Days Later/Character.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace _11100_Days_Later
+{
+    class Character : Actor
+    {
+        MouseState mouseState = Mouse.GetState();
+
+        InputHandler input = new InputHandler();
+
+
+        public Character(Vector2 startPosition, int startHealth)
+        {
+            position = startPosition;
+            health = startHealth;
+            speed = new Vector2(0);
+        }
+
+        public void LoadContent(ContentManager content)
+        {
+            sprite = content.Load<Texture2D>("Player");
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            MouseState mouseState = Mouse.GetState();
+            double yDiff = position.Y - mouseState.Y;
+            double xDiff = -(position.X - mouseState.X);
+
+            facing = (float)(90 - Math.Atan(yDiff / xDiff));
+
+
+        }
+    }
+}
